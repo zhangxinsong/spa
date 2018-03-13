cd6aab909d2a744d311eeb5bc11386142e20a791
zhangxinsong <648831686@qq.com>
2018-03-13 15:59:23 +0800

    change progressBar

--- 05-progressBar/index.js
+++ 05-progressBar/index.js
@@ -2,15 +2,19 @@ var progress = document.getElementById('progress');
 var start = document.getElementById('start');
 var stop = document.getElementById('stop');
 var rest = document.getElementById('rest');
-var timer;
+var timer = 0;
 var i = 0;
 
 start.onclick = function(){
+  if(timer !== 0){
+    return;
+  }
   timer = window.setInterval('doing()',100);
 }
 
 stop.onclick = function(){
   window.clearInterval(timer);
+  timer = 0;
 }
 
 rest.onclick = function(){
